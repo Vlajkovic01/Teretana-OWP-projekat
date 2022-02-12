04ae294e9c9f41fd57dda05694ac6f2086fd47e5
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2022-02-12 14:48:33 +0100

    Brisanje stavke iz korpe.

--- src/main/java/com/example/Teretana/Controller/KorisnickaKorpaController.java
+++ src/main/java/com/example/Teretana/Controller/KorisnickaKorpaController.java
@@ -1,9 +1,13 @@
 package com.example.Teretana.Controller;
 
+import com.example.Teretana.Model.Termin;
+import com.example.Teretana.Service.TerminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,10 +16,14 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/korisnickaKorpa")
 public class KorisnickaKorpaController implements ServletContextAware {
+
+    @Autowired
+    private TerminService terminService;
 
     @Autowired
     private ServletContext servletContext;
@@ -33,6 +41,18 @@ public class KorisnickaKorpaController implements ServletContextAware {
 
     @GetMapping("/pregled")
     public ModelAndView pregled(HttpSession session, HttpServletResponse response) throws IOException {
+        return new ModelAndView("mojaKorpa");
+    }
+
+    @PostMapping("/izbaci")
+    @SuppressWarnings("unchecked")
+    public ModelAndView izbaci(@RequestParam Long id,
+            HttpSession session, HttpServletResponse response) throws IOException {
+
+        List<Termin> terminiUKorpi = (List<Termin>) session.getAttribute(TerminiController.IZABRANI_TERMINI_ZA_KORPU);
+
+        terminiUKorpi.remove(terminService.findOne(id));
+        
         return new ModelAndView("mojaKorpa");
     }
 }
